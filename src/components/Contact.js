9cfd12119c58721843942ed5911028a1206d7333
AnthonyAnAn <anthony.almany@gmail.com>
2021-08-11 11:29:40 +1000

    navbar remake

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Button } from 'react-bootstrap';
 import { motion } from "framer-motion"
 
+
+
+
 function Contact() {
 
 
@@ -11,18 +14,26 @@ function Contact() {
         backgroundColor: "#7510F7"
     };
     return (
-        <div className="contact flex-center-column">
-            <div className="contact-container flex-center">
-                <h4>Interested to get in touch and make life more responsive?</h4>
-                <motion.div  whileHover={{ scale: 1.1 }}>
-                <div className="contact-button">
-                <Button variant="light" style={buttonStyle}>Let's do it!</Button>
-                </div>
-                  
-                </motion.div>
-            </div>
+ 
 
-        </div>
+
+
+     <div className="contact flex-center-column">
+         <div className="contact-container flex-center">
+             <h4>Interested to get in touch and make life more responsive?</h4>
+             <motion.div  whileHover={{ scale: 1.1 }}>
+             <div className="contact-button">
+             <Button variant="light" style={buttonStyle}>Let's do it!</Button>
+             </div>
+               
+             </motion.div>
+         </div>
+
+     </div>
+
+
+
+       
     )
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,32 +1,78 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {
     Link
 } from "react-router-dom";
 import {Link as Scroll} from 'react-scroll'
 
-import { Navbar, Nav } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import { ReactComponent as CloseMenu } from "../style/assets/x.svg";
+import { ReactComponent as MenuIcon } from "../style/assets/menu.svg";
+import { ReactComponent as Logo } from "../style/assets/logo.svg";
 
 
 function Navigation() {
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
     return (
 
-        <div>
+        <div className="header">
+       
+          <div className="logo-container">
+          <Scroll activeClass="home" to="home" spy={true} smooth={true}>
+          <Link to="/">  <button className="button-link"><Logo className="logo" /></button></Link>
+            </Scroll>
+          </div>
+          <div className="links-nav">
+          <ul className={click ? "nav-options active" : "nav-options"}>
 
-            <Navbar variant="dark" expand="lg" id="nav-flex" fixed="top" >
-            <Scroll activeClass="home" to="home" spy={true} smooth={true}>  <Link to="/">  <Navbar.Brand>Home</Navbar.Brand>  </Link> </Scroll>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="nav-flex-grow">
-                    <Nav className="mr-auto" id="flex-nav">
-                       <Scroll to="about" spy={true} smooth={true}> <Nav.Link>About</Nav.Link> </Scroll>
-                       <Scroll to="languages" spy={true} smooth={true}> <Nav.Link>Languages</Nav.Link> </Scroll>
-                       <Scroll to="projects" spy={true} smooth={true}> <Nav.Link>Projects</Nav.Link> </Scroll>
-                        <Link to="/api/sendmail">  <span id="contact">Contact</span> </Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+          <Scroll to="about" spy={true} smooth={true}>
+          <Link to="/">
+            <li className="option" onClick={closeMobileMenu}>
+              <button className="button-link">About</button>
+            </li>
+            </Link>
+            </Scroll>
+            <Scroll to="languages" spy={true} smooth={true}>
+          <Link to="/">
+            <li className="option" onClick={closeMobileMenu}>
+              <button className="button-link">Languages</button>
+            </li>
+            </Link>
+            </Scroll>
+            <Scroll to="projects" spy={true} smooth={true}>
+          <Link to="/">
+            <li className="option" onClick={closeMobileMenu}>
+              <button className="button-link">Projects</button>
+            </li>
+            </Link>
+            </Scroll>
+          <Link to="/api/sendmail">
+            <li className="option" onClick={closeMobileMenu}>
+              <button className="button-link">Contact</button>
+            </li>
+            </Link>
+  
+    
+
+          </ul>
+       
+     
+        <div className="mobile-menu" onClick={handleClick}>
+          {click ? (
+            <CloseMenu className="menu-icon" />
+          ) : (
+            <MenuIcon className="menu-icon" />
+          )}
         </div>
+        </div>
+      </div>
+     
     )
 }
 
 export default Navigation
+
