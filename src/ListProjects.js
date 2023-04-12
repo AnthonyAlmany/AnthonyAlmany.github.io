e7435a1e48e2fad74788575dfb638b7f04844d7d
AnthonyAlm <anthony.almany@gmail.com>
2023-04-12 16:54:23 +1000

    Add project

--- src/ListProjects.js
+++ src/ListProjects.js
@@ -1,15 +1,24 @@
 import orderProject from "./style/projects/order-project.png"
-import apiProject from "./style/projects/api-project.png"
+// import apiProject from "./style/projects/api-project.png"
 import landingPage from "./style/projects/main-background.jpg"
 import taskmanager from "./style/projects/task-project.png"
+import dashboard from "./style/projects/dashboard.png"
 
 const listProjects = [
     {
         title: "Easy`Order",
         description: "Inventory and orders management WebApp used by hospitality business to place orders easily.",
-        image: orderProject ,
+        image: orderProject,
         git: "https://github.com/anthonyAnAn/EasyOrder",
         view: "https://gifted-morse-f81260.netlify.app/",
+    },
+    {
+        title: "Dash Api",
+        description: "Scalable Api Dashboard WebApp using React, TypeScript, Axios and react-router.",
+        image: dashboard,
+        git: "https://github.com/AnthonyAlmany/Dashboard/tree/Layout",
+        view: "https://incandescent-salamander-ad6417.netlify.app/",
+
     },
     {
         title: "Task Manager",
@@ -18,14 +27,14 @@ const listProjects = [
         git: "https://github.com/AnthonyAlmany/Task-Manager",
         view: "https://marvelous-lily-c60d9b.netlify.app",
     },
-    {
-        title: "Dash Api",
-        description: "Scalable Api Dashboard WebApp using React, Axios and react-router.",
-        image: apiProject,
-        git: "https://github.com/anthonyAnAn/Api-App",
-        view: "https://boring-ardinghelli-a81f27.netlify.app/",
+    // {
+    //     title: "Dash Api",
+    //     description: "Scalable Api Dashboard WebApp using React, Axios and react-router.",
+    //     image: apiProject,
+    //     git: "https://github.com/anthonyAnAn/Api-App",
+    //     view: "https://boring-ardinghelli-a81f27.netlify.app/",
 
-    },
+    // },
     {
         title: "Landing Page",
         description: "My landing page using React, react-router, bootstrap, react-scroll and framer-motion",
